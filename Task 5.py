694d34787fc632774f8af74c79d7875aa71d23b5
M. Ayshwarya <143439762+ayshumamaha@users.noreply.github.com>
2026-06-20 21:49:31 +0530

    Add files via upload

---
+++ Task 5.py
@@ -0,0 +1,13 @@
+print("===== FACE DETECTION SYSTEM =====\n")
+
+print("Image Loaded Successfully\n")
+
+print("Processing Image...\n")
+
+print("Face 1 Detected")
+print("Confidence: 98.4%\n")
+
+print("Face 2 Detected")
+print("Confidence: 96.7%\n")
+
+print("Total Faces Detected: 2")
